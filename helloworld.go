30d151cb26b4437e6e0ee62fc8fbe656d1aa248d
okyraChan <okyra19chan@gmail.com>
2023-02-06 10:14:02 +0700

    upd: boolean

--- helloworld.go
+++ helloworld.go
@@ -4,4 +4,20 @@ import "fmt"
 
 func main() {
 	fmt.Println("Hello world")
+
+	// number
+	fmt.Println("======================================")
+	fmt.Println("NUMBER")
+	fmt.Println("Satu = ", 1)
+	fmt.Println("Dua = ", 2)
+	fmt.Println("Tiga Koma Lima = ", 3.5)
+	fmt.Println("======================================")
+
+	// boolean
+	fmt.Println("======================================")
+	fmt.Println("BOOLEAN")
+	fmt.Println("Benar = ", true)
+	fmt.Println("Salah = ", false)
+	fmt.Println("======================================")
+
 }
